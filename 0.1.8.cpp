7551914c9b256b592f6993e62d8ed451f9488f7c
Bohdan Konovalenko <60445869+Fagtoy@users.noreply.github.com>
2021-02-02 19:40:17 +0200

    Add files via upload

---
+++ 0.1.8.cpp
@@ -0,0 +1,14 @@
+/*Завдання 0.1.8, друга домашня робота*/
+#include <cstdio>
+#include <cmath>
+
+int main_8(){
+
+    double r, R;
+    printf("Input r and R: ");
+    scanf("%lf %lf", &r, &R);
+
+    float V = 2*M_PI*M_PI*R*r*r;
+    printf("V = %f", V);
+
+}

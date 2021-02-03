fcb99e3abfea82a95b7a4a651e0acfb2d52d48e5
Bohdan Konovalenko <60445869+Fagtoy@users.noreply.github.com>
2021-02-03 09:04:35 +0200

    Add files via upload

---
+++ class_rob.cpp
@@ -0,0 +1,15 @@
+double task_03_la(double x){
+    double z = x*x*x;
+    double y = z*z*z + z + 1;
+    return y;
+}
+
+int main{
+
+    double x;
+    scanf("%lf", &x);
+
+    y = task_03_la(x);
+    printf("%lf", y);
+
+}

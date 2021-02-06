7626dc513fa2b0bdc292ac71a761e27fb30b2eb4
Bohdan Konovalenko <60445869+Fagtoy@users.noreply.github.com>
2021-02-06 13:11:06 +0200

    Коноваленко Богдан, 3 дз

---
+++ dz_1.1.9.cpp
@@ -0,0 +1,33 @@
+// 1.1.9
+
+#include <cstdio>
+
+int main() {
+
+    double x, y, r, a, b, c;
+    int count = 0;
+    printf("x y r a b c: ");
+    scanf("%lf %lf %lf %lf %lf %lf", &x, &y, &r, &a, &b, &c);
+
+    if (x != a){
+        printf("x must be equal to a");
+    } else if (y <= b or y >= b + c*c){
+        printf("b <= y <= b + c*c");
+    }
+
+    if (x == r){
+        count++;
+    } else if (x <= r){
+        count += 2;
+    }
+
+    if (b == r or (b == -r and b + c*c >= r)){
+        count++;
+    } else if (b == -r and b + c*c == r){
+        count += 3;
+    } else if (b == -r and b + c*c == r) {
+        count += 2;
+    }
+
+    printf("%d", count);
+}

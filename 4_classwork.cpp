bf87f5878c6a0c8dace5d0397e49b9ce9015c4b9
Bohdan Konovalenko <60445869+Fagtoy@users.noreply.github.com>
2021-02-08 10:01:15 +0200

    Коноваленко Богдан, класна робота, цикли

---
+++ 4_classwork.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+
+    double y,x;
+    unsigned n;
+    printf("x=");
+    scanf("%lf", &x);
+    printf("\nn=");
+    scanf("%u", &n);
+
+    y = 1;
+    int power = 1;
+
+    double y_1=1;
+    int power_1 = 1;
+
+    for (int i=0; i<n; i++){
+        power *= x*x;
+        y += power;
+    }
+
+    for (int i=0; i<n; i++){
+        power_1 *= x*x*x;
+        y_1 += power;
+    }
+
+    printf("%lf", y);
+    printf("\n%lf", y_1);
+
+}
